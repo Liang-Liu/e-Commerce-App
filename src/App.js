49e0d5b4021175d6f1226187b8196e3460e91246
Liang-Liu <82113781+Liang-Liu@users.noreply.github.com>
2021-08-05 23:03:09 -0700

    feature 03 added shopping cart section

--- src/App.js
+++ src/App.js
@@ -2,46 +2,123 @@ import { useState } from "react";
 import originalData from "./data.json";
 import ProductCell from "./components/ProductCell";
 import FilterBar from "./components/FilterBar";
+import CartCell from "./components/CartCell";
 
 const productData = originalData;
 
 function App() {
-	const [data, setData] = useState(productData);
+	const [data, setData] = useState({
+		products: productData.products,
+		cartItems: [],
+	});
 
 	function sortProduct(e) {
 		const sortValue = e.target.value;
 		if (sortValue === "latest") {
-			setData(productData);
+			setData((prevData) => {
+				return {
+					...prevData,
+					products: productData.products,
+				};
+			});
 		} else if (sortValue === "lowest") {
 			function compareFunc(a, b) {
 				return a.price - b.price;
 			}
 			const tempArr = data.products;
 			const nextData = [...tempArr].sort(compareFunc);
-			const nextState = { products: nextData };
-			setData(nextState);
+			setData((prevData) => {
+				return {
+					...prevData,
+					products: nextData,
+				};
+			});
 		} else if (sortValue === "highest") {
 			function compareFunc(a, b) {
 				return b.price - a.price;
 			}
 			const tempArr = data.products;
 			const nextData = [...tempArr].sort(compareFunc);
-			const nextState = { products: nextData };
-			setData(nextState);
+			setData((prevData) => {
+				return {
+					...prevData,
+					products: nextData,
+				};
+			});
 		}
 	}
-
 	function FilterProduct(e) {
 		const filterValue = e.target.value;
 		if (filterValue === "ALL") {
-			setData(productData);
+			setData((prevData) => {
+				return {
+					...prevData,
+					products: productData.products,
+				};
+			});
 		} else {
 			const nextData = productData.products.filter((ele, idx) =>
 				ele.availableSizes.includes(filterValue)
 			);
-			const nextState = { products: nextData };
-			setData(nextState);
+			setData((prevData) => {
+				return {
+					...prevData,
+					products: nextData,
+				};
+			});
 		}
+	}
+	function addToCart(item) {
+		// console.log(item);
+		function someCallBackFunc(ele, idx) {
+			return ele.item.id === item.id;
+		}
+
+		if (!data.cartItems.some(someCallBackFunc)) {
+			setData((prevData) => {
+				const cartItem = { item: item, count: 1 };
+				return {
+					...prevData,
+					cartItems: [...prevData.cartItems, cartItem],
+				};
+			});
+		}
+		if (data.cartItems.some(someCallBackFunc)) {
+			const temp = data.cartItems.map((ele, idx) => {
+				if (ele.item.id === item.id) {
+					ele.count++;
+				}
+				return ele;
+			});
+			setData((prevData) => {
+				return {
+					...prevData,
+					cartItems: temp,
+				};
+			});
+		}
+	}
+	function removeFromCart(item) {
+		console.log(item);
+		const temp = data.cartItems.filter((ele, idx) => {
+			console.log(ele);
+
+			return ele !== item;
+		});
+
+		setData((prevData) => {
+			return {
+				...prevData,
+				cartItems: temp,
+			};
+		});
+	}
+	function countTotalPrice(arr) {
+		let totalPrice = 0;
+		arr.forEach((ele, idx) => {
+			totalPrice += ele.item.price * ele.count;
+		});
+		return totalPrice;
 	}
 
 	return (
@@ -56,11 +133,42 @@ function App() {
 				<main>
 					Main
 					{data.products.map((ele, idx) => {
-						// console.log(ele);
-						return <ProductCell key={ele.id} cellData={ele} />;
+						return (
+							<ProductCell
+								key={ele.id}
+								cellData={ele}
+								addToCartFunc={addToCart}
+							/>
+						);
 					})}
 				</main>
-				<div className="sidebar">shopping cart</div>
+				<div className="sidebar">
+					shopping cart
+					<h3>You have {data.cartItems.length} item in cart</h3>
+					{console.log(data.cartItems)}
+					{data.cartItems.map((ele, idx) => {
+						return (
+							<CartCell
+								key={ele.item.id}
+								cartItemData={ele}
+								removeFromCartFunc={removeFromCart}
+							/>
+						);
+					})}
+					{data.cartItems.length !== 0 && (
+						<div className="proceedSection">
+							<h4>Total: $ {countTotalPrice(data.cartItems)}</h4>
+							<button
+								className="proceed"
+								onClick={(e) => {
+									// addToCartFunc(cellData);
+								}}
+							>
+								Proceed
+							</button>
+						</div>
+					)}
+				</div>
 			</div>
 
 			<footer>footer</footer>
--- src/components/ProductCell.js
+++ src/components/ProductCell.js
@@ -1,14 +1,21 @@
 import React from "react";
 
-function ProductCell({
-	cellData: { id, image, title, description, availableSizes, price },
-}) {
+function ProductCell({ cellData, addToCartFunc }) {
+	const { id, image, title, description, availableSizes, price } = cellData;
+
 	return (
 		<div className="productCell-container">
 			<img className="product" src={image} alt="id" />
 			<div className="title">{title}</div>
 			<div className="price">${price}</div>
-			<button className="addToCart">Add To Cart</button>
+			<button
+				className="addToCart"
+				onClick={(e) => {
+					addToCartFunc(cellData);
+				}}
+			>
+				Add To Cart
+			</button>
 		</div>
 	);
 }
